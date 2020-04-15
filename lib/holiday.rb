9d56a1831e1accd98057f608dd20ec6c4bc4f783
Loren Cipriano <loren25cipriano@gmail.com>
2020-04-15 01:53:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -94,9 +94,6 @@ end
 
 
 
-holiday_hash.each do |season, holiday_name_hash|
-   print holiday_hash
-
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
